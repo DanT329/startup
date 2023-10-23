160a7e2464fbc0e783c6ca7a2bd4d2b3cbf1fa1d
DanT329 <dterry16@byu.edu>
2023-10-23 14:35:35 -0600

    stores information when clicking message on users.

--- users.js
+++ users.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-   
     var name = localStorage.getItem('name');
     document.querySelector('header p').innerText = name;
 
@@ -11,11 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
             <p>Level: ${localStorage.getItem('experienceLevel')}</p>
             <p>Type: ${localStorage.getItem('workoutType')}</p>
             <p>User Rating: ★★★★☆</p>
-            <button type="button">Message</button>
+            <button type="button" class="message-button">Message</button>
         </div>
     `;
 
     for (var i = 0; i < 6; i++) {
         main.innerHTML += cardTemplate;
     }
+
+    var messageButtons = document.querySelectorAll('.message-button');
+    messageButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var card = this.parentElement;
+            localStorage.setItem('cardName', card.querySelector('h2').innerText);
+            localStorage.setItem('cardTime', card.querySelector('p').innerText);
+            localStorage.setItem('cardLevel', card.querySelectorAll('p')[1].innerText);
+            localStorage.setItem('cardType', card.querySelectorAll('p')[2].innerText);
+            localStorage.setItem('cardRating', card.querySelectorAll('p')[3].innerText);
+            window.location.href = 'messages.html';
+        });
+    });
 });
+
