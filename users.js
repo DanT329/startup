ca5d7f6ffdbfd9e83e0b0a837b090b1ece6ac5cd
DanT329 <dterry16@byu.edu>
2023-10-25 10:28:01 -0600

    Added a place holder array and remove card on click

--- users.js
+++ users.js
@@ -3,20 +3,50 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('header p').innerText = name;
 
     var main = document.querySelector('main');
-    var cardTemplate = `
-        <div class="profile-card">
-            <h2>Name: ${name}</h2>
-            <p>Time: ${localStorage.getItem('time')}</p>
-            <p>Level: ${localStorage.getItem('experienceLevel')}</p>
-            <p>Type: ${localStorage.getItem('workoutType')}</p>
-            <p>User Rating: ★★★★☆</p>
-            <button type="button" class="message-button">Message</button>
-        </div>
-    `;
 
-    for (var i = 0; i < 3; i++) {
+    // Place holder array of user data
+    var usersData = [
+        {
+            name: 'User1',
+            time: '07:00AM',
+            experienceLevel: 'Beginner',
+            workoutType: 'Cardio',
+            rating: '★★★☆☆'
+        },
+        {
+            name: 'User2',
+            time: '04:00AM',
+            experienceLevel: 'Intermediate',
+            workoutType: 'Cardio',
+            rating: '★★★★☆'
+        },
+        {
+            name: 'User3',
+            time: '09:00AM',
+            experienceLevel: 'Advanced',
+            workoutType: 'Srength',
+            rating: '★★★★★'
+        }
+    ];
+    
+    localStorage.setItem('usersData', JSON.stringify(usersData));
+    
+    var cardsData = JSON.parse(localStorage.getItem('usersData')) || [];
+
+    cardsData.forEach(function(cardData) {
+        var cardTemplate = `
+            <div class="profile-card">
+                <h2>Name: ${cardData.name}</h2>
+                <p>Time: ${cardData.time}</p>
+                <p>Level: ${cardData.experienceLevel}</p>
+                <p>Type: ${cardData.workoutType}</p>
+                <p>User Rating: ★★★★☆</p>
+                <button type="button" class="message-button">Message</button>
+            </div>
+        `;
+
         main.innerHTML += cardTemplate;
-    }
+    });
 
     var messageButtons = document.querySelectorAll('.message-button');
     messageButtons.forEach(function(button) {
@@ -27,6 +57,7 @@ document.addEventListener('DOMContentLoaded', function() {
             localStorage.setItem('cardLevel', card.querySelectorAll('p')[1].innerText);
             localStorage.setItem('cardType', card.querySelectorAll('p')[2].innerText);
             localStorage.setItem('cardRating', card.querySelectorAll('p')[3].innerText);
+            card.remove();
             window.location.href = 'messages.html';
         });
     });
