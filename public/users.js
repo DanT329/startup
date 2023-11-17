fdf0301e44463ee08b656282d1211deb6a83ae6e
DanT329 <dterry16@byu.edu>
2023-11-16 19:43:43 -0700

    users page now redirects if not authorized

--- public/users.js
+++ public/users.js
@@ -6,35 +6,49 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     fetch('/api/UsersData')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return response.json();
+    })
     .then(cardsData => {
-        cardsData.forEach(function(cardData) {
-            var cardTemplate = `
-                <div class="profile-card">
-                    <h2>${cardData.name}</h2>
-                    <p>Time: ${cardData.time}</p>
-                    <p>Level: ${cardData.experienceLevel}</p>
-                    <p>Type: ${cardData.workoutType}</p>
-                    <p>User Rating: ★★★★☆</p>
-                    <button type="button" class="message-button">Message</button>
-                </div>
-            `;
-
-            main.innerHTML += cardTemplate;
+      cardsData.forEach(function(cardData) {
+        var cardTemplate = `
+          <div class="profile-card">
+              <h2>${cardData.name}</h2>
+              <p>Time: ${cardData.time}</p>
+              <p>Level: ${cardData.experienceLevel}</p>
+              <p>Type: ${cardData.workoutType}</p>
+              <p>User Rating: ★★★★☆</p>
+              <button type="button" class="message-button">Message</button>
+          </div>
+        `;
+    
+        main.innerHTML += cardTemplate;
+      });
+    
+      // Adding message button event handlers after all profile cards are added
+      var messageButtons = document.querySelectorAll('.message-button');
+      messageButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+          var card = this.parentElement;
+          var receiverName = card.querySelector('h2').innerText;
+          var senderName = localStorage.getItem('newUserName'); // Replace this with the actual sender's name
+          window.location.href = 'messages.html?receiver=' + encodeURIComponent(receiverName) + '&sender=' + encodeURIComponent(senderName);
+          localStorage.setItem('receiver', receiverName );
+          console.log('receiver');
         });
-
-        var messageButtons = document.querySelectorAll('.message-button');
-        messageButtons.forEach(function(button) {
-            button.addEventListener('click', function() {
-                var card = this.parentElement;
-                var receiverName = card.querySelector('h2').innerText;
-                var senderName = localStorage.getItem('newUserName'); // Replace this with the actual sender's name
-                window.location.href = 'messages.html?receiver=' + encodeURIComponent(receiverName) + '&sender=' + encodeURIComponent(senderName);
-                localStorage.setItem('receiver', receiverName );
-                console.log('receiver');
-            });
-        });
+      });
+    })
+    .catch(error => {
+        if (error.message == '401') {
+            window.location.href = 'login.html';
+        }
     });
-});
+
+      
+    });
+
 
 
