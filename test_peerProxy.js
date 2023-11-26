23e950d9f5a99b6f5c89251906a12edb259009fb
DanT329 <dterry16@byu.edu>
2023-11-26 11:34:58 -0700

    user leaves

--- test_peerProxy.js
+++ test_peerProxy.js
@@ -47,4 +47,10 @@ function activateUser(id, name, room){
   ])
   return user
 }
+
+function userLeavesApp(id){
+  UsersState.setUsers(
+    UsersState.users.filter(user => user.id !==id)
+  )
+}
 module.exports = { peerProxy };
