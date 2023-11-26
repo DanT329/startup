ee633ba1c07a4bcd996edb56876a7a524bb484e7
DanT329 <dterry16@byu.edu>
2023-11-26 12:29:09 -0700

    edit the websocket server

--- public/test_message.js
+++ public/test_message.js
@@ -35,9 +35,44 @@ document.querySelector('.form-join').addEventListener('submit', enterRoom)
 
 // Listen for Messages
 socket.on('message',(data) => {
+  const {name, text, time} = data
   const li = document.createElement('li')
-  li.textContent = data
-  document.querySelector('ul').appendChild(li)
+  li.className = 'post'
+  if(name === sender) li.className = 'post post--left'
+  if (name !== sender && name !== 'Admin') li.className = 'post post--right'
+
+  if(name !== 'Admin'){
+    li.innerHTML = `<dive class="post__header ${name === sender? 'poast__header--user': 'post__header--reply' }"><span class="post__header--name">${name}</span><span class="post__header--time">${time}</span></div><div class="post__text">${text}</div>`
+  } else{
+    li.innterHTML = `<div class="post__text">${text}</div>`
+  }
+  document.querySelector('.chat-display').appendChild(li)
+
+  chatDisplay.scrollTop = chatDisplay.scrollHeight
 })
 
+function showUsers(users){
+  usersList.textContent = ''
+  if(users){
+    usersList.innterHTML = `<em>Users in ${chatRoom.value}:</em>`
+    users.forEach((user, i) =>{
+      usersList.textContent += `${user.name}`
+      if(user.length > 1 && i !== users.length - 1){
+        usersList.textContent += ","
+      }
+    })
+  }
+}
 
+function showRooms(rooms){
+  roomList.textContent = ''
+  if(rooms){
+    roomList.innterHTML = `<em>Active Rooms:</em>`
+    rooms.forEach((room, i) =>{
+      roomList.textContent += `${room}`
+      if(rooms.length > 1 && i !== rooms.length - 1){
+        roomList.textContent += ","
+      }
+    })
+  }
+}
